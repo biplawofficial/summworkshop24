fb297249be16798fdba6e9c941ad5e727a86390b
sage <udaydebnath999999@gmail.com>
2024-06-08 09:35:26 +0530

    day one

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mii map<int,int>
+#define ct() int _;cin>>_;while(_--)
+#define vi vector<int>
+int main()
+{
+    int n,i;
+    cin>>n;
+    vi arr(n);
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+    int l=0,r=0;
+    int count=0,temp=0;
+    while(r<n)
+    {
+        
+    }
+
+}
