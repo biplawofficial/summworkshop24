e7d35c2bf1c5e49b1eded6491498f765d7519ee3
sage <udaydebnath999999@gmail.com>
2024-06-11 17:40:47 +0530

    day 4

--- react/frontend/appp/src/components/Home/Home.jsx
+++ react/frontend/appp/src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Home.css'
+import Navbar1 from './navbar1/navbar1';
 const Home=()=>
     {
-        return (<div>
-            <p>Lorem ipsum</p>
-        </div>)
+        return (
+     <div className="home">
+        <Navbar1/>
+     </div>
+        )
     }
 
     export default Home
